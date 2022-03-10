f2386fc8823aabbce1501b18d1b05eddea751c3d
Abdulrasheed <abdrasheed1103@gmail.com>
2022-03-10 12:23:49 +0100

    NFTs

--- Hardhat/scripts/MaxiNFT.ts
+++ Hardhat/scripts/MaxiNFT.ts
@@ -8,7 +8,7 @@ const MaxiNFT = await ethers.getContractFactory("MaxiNFT");
 const maxinft = await MaxiNFT.deploy();
 await maxinft.deployed();
 
-await maxinft.mintNFT("0xe66904a5318f27880bf1d20D77Ffa8FBdaC5E5E7", "ipfs://QmVuUUos55Rd9X9M2FmjfX88Ef7LNN5JiNWb3AM5okWvSn")
+await maxinft.mintNFT("0xe66904a5318f27880bf1d20D77Ffa8FBdaC5E5E7", "ipfs://QmUDJav6jfUB6JZuwn1sSKDgFDg9WwUj8LDeU3xoRiZNfy")
     
     console.log(await maxinft.address)
 }
