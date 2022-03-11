5426bd64b36ef0a5e5bbece11346d6f0b73471ba
Abdulrasheed <abdrasheed1103@gmail.com>
2022-03-11 23:57:42 +0100

    comments

--- Hardhat/scripts/MaxiNFT.ts
+++ Hardhat/scripts/MaxiNFT.ts
@@ -3,9 +3,10 @@ import { ethers } from "hardhat";
 
 
 async function deployNFT() {
-
+// get contract
 const MaxiNFT = await ethers.getContractFactory("MaxiNFT");
-const maxinft = await MaxiNFT.deploy();
+// Deploy contract
+  const maxinft = await MaxiNFT.deploy();
 await maxinft.deployed();
 
 await maxinft.mintNFT("0xe66904a5318f27880bf1d20D77Ffa8FBdaC5E5E7", "ipfs://QmUDJav6jfUB6JZuwn1sSKDgFDg9WwUj8LDeU3xoRiZNfy")
