cb399091cfff27e731365fe92f25cf343f3d162c
Abdulrasheed <abdrasheed1103@gmail.com>
2022-03-11 23:58:43 +0100

    Comments

--- Hardhat/scripts/MaxiNFT.ts
+++ Hardhat/scripts/MaxiNFT.ts
@@ -8,7 +8,7 @@ const MaxiNFT = await ethers.getContractFactory("MaxiNFT");
 // Deploy contract
   const maxinft = await MaxiNFT.deploy();
 await maxinft.deployed();
-
+// Address to and NFT URI
 await maxinft.mintNFT("0xe66904a5318f27880bf1d20D77Ffa8FBdaC5E5E7", "ipfs://QmUDJav6jfUB6JZuwn1sSKDgFDg9WwUj8LDeU3xoRiZNfy")
     
     console.log(await maxinft.address)
