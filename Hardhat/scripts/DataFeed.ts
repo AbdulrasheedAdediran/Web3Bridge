0bcb1cc09dab2078f3452bcc9824f95bc6cfe0a4
Abdulrasheed <abdrasheed1103@gmail.com>
2022-03-15 15:31:10 +0100

    Data Feed

--- Hardhat/scripts/DataFeed.ts
+++ Hardhat/scripts/DataFeed.ts
@@ -1,7 +1,7 @@
 import { ethers } from "hardhat";
 
 async function deployContract(){
-    // const AVAXtoUSD	= "0x5498BB86BC934c8D34FDA08E81D444153d0D06aD";
+   
     const ATOMtoETH	=	"0xc751E86208F0F8aF2d5CD0e29716cA7AD98B5eF5";
     const DataFeed = await ethers.getContractFactory("PriceConsumerV3");
     const datafeed = await DataFeed.deploy(ATOMtoETH)
